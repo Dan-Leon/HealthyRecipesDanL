885c7e01d14d9a1cf441fa43a8d5f3c9ba6bfc9a
dleon <dleon@students.mchenry.edu>
2019-09-01 23:08:22 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/firstapp/healthyrecipes/Recipe.java
@@ -0,0 +1,14 @@
+package com.example.android.firstapp.healthyrecipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+    }
+}
